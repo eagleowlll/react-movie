095445480f2b5aa4bcf9cb98511cd58f2305407d
eagleowlll <1894citizenwon@gmail.com>
2025-04-01 14:06:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
